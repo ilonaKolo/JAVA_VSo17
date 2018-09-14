97d97224a33df4998b00849770f6de6e8855da62
ilona <ilona.kolossova@khk.ee>
2018-09-14 14:55:54 +0300

    Täisarvulised operatsioonid

---
+++ src/java/esimene/Main.java
@@ -0,0 +1,13 @@
+package java.esimene;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Ilona!");
+
+
+        int myFirstNumber = (10 + 5) * 2;
+        int mySecondNumber = (7 + 5) * 4;
+        System.out.println(myFirstNumber);
+        System.out.println(mySecondNumber);
+    }
+}
