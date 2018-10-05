69cd09789b8aa68259f68ebcc9c15a18cda99d39
IlonaKolo <ilona.kolossova@khk.ee>
2018-10-05 16:08:13 +0300

    "Ring" faili loomine

---
+++ src/java/teine/Ring.java
@@ -0,0 +1,32 @@
+package kool;
+
+public class Ring {
+    public double raadius;
+    public String varv;
+
+    public Ring() {
+        this.raadius = 0.0;
+        this.varv = "värvitu";
+    }
+
+    public Ring(double raadius) {
+        this.raadius = raadius;
+        this.varv = "värvitu";
+    }
+
+    public Ring(double raadius, String varv) {
+        this.raadius = raadius;
+        this.varv = varv;
+    }
+
+    public double ringjoonePikkus() {
+        return 2 * Math.PI * this.raadius;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Antud ring on raadiusega " +
+                raadius + " ja " + varv + " värviga";
+    }
+}
